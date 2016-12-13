179555783dc56a7b10832445e431ec5fbf7b79f1
sjkhullar <lovetoyogesh@gmail.com>
2016-12-14 01:36:05 +0530

    Update SerializeDeserialize.java

--- ExternalizeInJava/src/main/java/org/learn/SerializeDeserialize.java
+++ ExternalizeInJava/src/main/java/org/learn/SerializeDeserialize.java
@@ -18,18 +18,21 @@ public class SerializeDeserialize {
 		outputStream.writeObject(person);
 		outputStream.flush();
 		outputStream.close();
-		System.out.println("Serialized the person object : "+person);
-		
-		//Update the static variable to confirm that its not serialized
-		Person.lastName = "last name after serialization";
-	} 
+
+		System.out.println("1. Serialized the person object : ");
+		System.out.println(person);
+		System.out.println("LastName:"+Person.lastName);
+	}
 	public static void deSerialize() throws IOException, ClassNotFoundException {
 		
 		FileInputStream inputStream = new FileInputStream(new File("savePerson.txt"));
 		ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 		Person person = (Person) objectInputStream.readObject();
-		System.out.println("Deserialize the person object :"+ person);
 		inputStream.close();
+
+		System.out.println("\n2. Deserialized person object : ");
+		System.out.println(person);
+		System.out.println("LastName:"+Person.lastName);
 	}
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
